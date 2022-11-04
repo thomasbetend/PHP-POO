c60f155e48f90c3517e5933b9a200d64906a97df
thomasbetend <thomas.betend@yahoo.fr>
2022-11-04 11:32:53 +0100

    Challenge 6 accepted

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
-require_once('Car.php');
-require_once('Bicycle.php');
-require_once('Skateboard.php');
+require_once('Speedometer.php');
 
-$tesla = new Car('red', 5, 'electric');
-$peugeot208CC = new Car('blue', 4, 'gasoil');
-$triporteur = new Bicycle('grey', 4);
-$longBoard = new Skateboard('black', 1);
-
-$triporteur->setCurrentSpeed(10);
-
-var_dump($longBoard);
-var_dump($triporteur); 
-
-var_dump($triporteur->switchOn());
-
-var_dump($tesla->switchOn());
-
-
-
-
-
-
+echo Speedometer::convertKmToMile(10);
