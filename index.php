584890c03f30817b40030e000f3bef74f4120b38
thomasbetend <thomas.betend@yahoo.fr>
2022-11-04 10:19:06 +0100

    Challenge 3 accepted

--- index.php
+++ index.php
@@ -1,28 +1,51 @@
 <?php
-
-require_once('Bicycle.php');
+require_once('MotorWay.php');
+require_once('ResidentialWay.php');
+require_once('PedestrianWay.php');
 require_once('Car.php');
-require_once('Truck.php');
+require_once('Bicycle.php');
+require_once('Skateboard.php');
 
-$bike = new Bicycle('red', 15);
-$car1 = new Car('blue', 3, 'SP95');
+$tesla = new Car('red', 5, 'electric');
+$peugeot208CC = new Car('blue', 4, 'gasoil');
+$triporteur = new Bicycle('grey', 4);
+$longBoard = new Skateboard('black', 1, 120);
 
-$truck1 = new Truck('green', 3, 'gasoil', 40);
+$motorwaySun = new MotorWay(4, 130);
+$residentialWaySeaView = new ResidentialWay(2, 50);
+$pedestrianWayWoody = new PedestrianWay(1,10);
 
-$truck1->setLoading(78);
+echo $motorwaySun->addVehicle($tesla) . '<br>';
+echo $motorwaySun->addVehicle($peugeot208CC) . '<br>';
+echo $motorwaySun->addVehicle($triporteur) . '<br>';
 
-$truck1->setCurrentSpeed(80);
+echo $residentialWaySeaView->addVehicle($triporteur) . '<br>';
+echo $residentialWaySeaView->addVehicle($longBoard) . '<br>';
 
-echo $truck1->fillingTruck();
-echo "<br>" . $truck1->start();
-$truck1->setCurrentSpeed(80);
-
-echo "<br>" . $truck1->forward();
-echo "<br>" . $truck1->brake();
-
-var_dump($truck1);
-
-
-
+echo $pedestrianWayWoody->addVehicle($triporteur) . '<br>';
+echo $pedestrianWayWoody->addVehicle($tesla) . '<br>';
 
 
+var_dump($motorwaySun); 
+var_dump($residentialWaySeaView); 
+var_dump($pedestrianWayWoody);
+
+
+
+
+
+
+
+
+/* $batmans = ['robin', 'batman', 'joker'];
+
+function bat($array){
+
+    foreach($array as $element){
+        return $element;
+    }
+
+}
+
+echo bat($batmans);
+ */
